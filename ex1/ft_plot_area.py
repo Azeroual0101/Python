8f4bfddf4263b07999282972fbde5602e909dd06
Abderrahim Azeroual <abdazero@e2r11p1.1337.ma>
2026-03-01 02:32:02 +0000

    okk

---
+++ ex1/ft_plot_area.py
@@ -0,0 +1,9 @@
+def ft_plot_area():
+    area = 0
+    length = 0
+    width = 0
+
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    area = length * width
+    print("Plot area: ",area)
---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,8 @@
+def ft_harvest_total():
+    i = 1
+    totale = 0
+    while i < 4:
+        day = int(input(f"Day {i} harvest: "))
+        totale += day
+        i += 1
+    print("Total harvest: ",totale) 
